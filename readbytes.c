2563398783fad874d64d42690716a7b78f6d950c
Tommy Unger <tunger10@gmail.com>
2021-12-21 14:24:13 -0500

    Hack read to elevate

--- readbytes.c
+++ readbytes.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include "Symbi-OS/Apps/include/headers/sym_lib.h"
+#include "Symbi-OS/Apps/include/headers/sym_lib_hacks.h"
 
 void
 usage(int argc, char **argv)
@@ -27,24 +28,30 @@ main(int argc, char **argv)
   uint64_t addr = strtoull(argv[1],NULL,16);
   int len = atoi(argv[2]);
 
+  char* buff = malloc(len);
+
+  sym_touch_stack();
+  sym_touch_every_page_text();
   rc = sym_elevate();
-  if (rc<0) {    
-    fprintf(stderr, "ERROR: failed to sym_elevate: rc=%ld\n", rc);
-    exit(-1);
-  }
+
+  memcpy(buff, (void *)addr, len);
+  /* if (rc<0) {     */
+  /*   fprintf(stderr, "ERROR: failed to sym_elevate: rc=%ld\n", rc); */
+  /*   exit(-1); */
+  /* } */
   
-  rc = write(STDOUT_FILENO, (void *)addr, len);
-  if (rc != len) {    
+  rc = write(STDOUT_FILENO, (void *)buff, len);
+  if (rc != len) {
     fprintf(stderr, "WARNING: failed to read %d : rc=%ld\n",len, rc);
     ec = -1;
   }
   
   rc = sym_lower();
   
-  if (rc<0) {    
-    fprintf(stderr, "ERROR: failed to sym_lower: rc=%ld\n", rc);
-    ec = -1;
-  }
+  /* if (rc<0) {     */
+  /*   fprintf(stderr, "ERROR: failed to sym_lower: rc=%ld\n", rc); */
+  /*   ec = -1; */
+  /* } */
   
   return ec;
 }
